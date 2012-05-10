d7cc2ef72dc3b37f7733698f2eae780e31576cfa
Valder Gallo <valdergallo@gmail.com>
2012-05-10 01:36:34 -0300

    [bug] daemon doest work

--- twitterbeat/__init__.py
+++ twitterbeat/__init__.py
@@ -47,11 +47,13 @@ class TwitterBeat(Thread):
     
     def __init__(self):
         threading.Thread.__init__(self)
+        
         self._is_shutdown = Event()
         self._is_stopped = Event()
+        
         self.KeepAlive = True
         self.count_runner = 0
-        self.daemon = True
+        #self.daemon = True
         
         user_id  = getattr(settings, 'TWITTER_USER_ID', 
                             Account.objects.filter(active=True).latest('id').id)
@@ -63,10 +65,12 @@ class TwitterBeat(Thread):
     def stop(self):
         self.KeepAlive = False 
         self._is_shutdown.set()
-        #self._is_stopped.wait()
-        sys.stdout.write('STOOPED \n')
+        self._is_stopped.set()
+        #sys.stdout.write('STOOPED \n')
+        print 'STOOPED \n'
         if self.is_alive():
-            sys.stdout.write('IS ALIVE \n')
+            #sys.stdout.write('IS ALIVE \n')
+            print 'IS ALIVE \n'
             self.join(1e100)
 
     @staticmethod
@@ -84,31 +88,8 @@ class TwitterBeat(Thread):
         fmt = '%a %b %d %H:%M:%S +0000 %Y' #Tue Apr 26 08:57:55 +0000 2011
         return datetime.strptime(created_at_string, fmt)
         
-    def _passe_rss(self):
+    def parse_rss(self):
         """
-        In [12]: p.entries[0]
-        Out[12]: 
-        {'guidislink': True,
-         'id': u'http://twitter.com/valdergallo/statuses/198914864721309696',
-         'link': u'http://twitter.com/valdergallo/statuses/198914864721309696',
-         'links': [{'href': u'http://twitter.com/valdergallo/statuses/198914864721309696',
-           'rel': u'alternate',
-           'type': u'text/html'}],
-         'published': u'Sat, 05 May 2012 23:19:34 +0000',
-         'published_parsed': time.struct_time(tm_year=2012, tm_mon=5, tm_mday=5, tm_hour=23, tm_min=19, tm_sec=34, tm_wday=5, tm_yday=126, tm_isdst=0),
-         'summary': u'valdergallo: Algu\xe9m \xe9 cliente HSBC para comprar para mim ? - Unisonic Very Special Guest: Gotthard: http://t.co/B5TXnbyg',
-         'summary_detail': {'base': u'http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=valdergallo',
-          'language': None,
-          'type': u'text/html',
-          'value': u'valdergallo: Algu\xe9m \xe9 cliente HSBC para comprar para mim ? - Unisonic Very Special Guest: Gotthard: http://t.co/B5TXnbyg'},
-         'title': u'valdergallo: Algu\xe9m \xe9 cliente HSBC para comprar para mim ? - Unisonic Very Special Guest: Gotthard: http://t.co/B5TXnbyg',
-         'title_detail': {'base': u'http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=valdergallo',
-          'language': None,
-          'type': u'text/plain',
-          'value': u'valdergallo: Algu\xe9m \xe9 cliente HSBC para comprar para mim ? - Unisonic Very Special Guest: Gotthard: http://t.co/B5TXnbyg'},
-         u'twitter_place': u'',
-         u'twitter_source': u'<a href="http://twitter.com/tweetbutton" rel="nofollow">Tweet Button</a>'}
-        
         #fields
             t.entries[0].published_parsed #created_at
             t.entries[0].link #urls
@@ -116,6 +97,7 @@ class TwitterBeat(Thread):
             t.entries[0].summary #text
             t.entries[0].title #title
         """
+        print 'GET RSS'
         tweets = feedparser.parse(self.twitter_rss)
         parsed = []
         for tweet in tweets.entries:
@@ -127,33 +109,8 @@ class TwitterBeat(Thread):
             })
         return parsed
     
-    def _parse_tweet(self):
+    def parse_tweet(self):
         """
-        In [25]: tweets[0].__dict__
-        Out[25]: 
-        {'_contributors': None,
-         '_coordinates': None,
-         '_created_at': u'Sat May 05 23:19:34 +0000 2012',
-         '_favorited': False,
-         '_geo': None,
-         '_id': 198914864721309696,
-         '_in_reply_to_screen_name': None,
-         '_in_reply_to_status_id': None,
-         '_in_reply_to_user_id': None,
-         '_location': None,
-         '_now': None,
-         '_place': None,
-         '_retweet_count': 0,
-         '_retweeted': False,
-         '_retweeted_status': None,
-         '_source': u'<a href="http://twitter.com/tweetbutton" rel="nofollow">Tweet Button</a>',
-         '_text': u'Algu\xe9m \xe9 cliente HSBC para comprar para mim ? - Unisonic Very Special Guest: Gotthard: http://t.co/B5TXnbyg',
-         '_truncated': False,
-         '_user': <twitter.User at 0x10c321ed0>,
-         'hashtags': None,
-         'urls': None,
-         'user_mentions': None}
-        
          #fields 
             tweet.text
             tweet.created_at
@@ -161,12 +118,13 @@ class TwitterBeat(Thread):
             tweet.user.name
             tweet.id
         """
+        print 'GET API'
         api = twitter.Api()
         try:
             tweets = api.GetUserTimeline(self.twitter_user, count=10)
         except Exception, e:
             ConectionError.objects.create(text=e)
-
+            return self.parse_rss()
         parsed = []
         for tweet in tweets:
             parsed.append({
@@ -178,9 +136,9 @@ class TwitterBeat(Thread):
         return parsed
     
     def run(self):
-        sys.stdout.write('Getting Twitter \n')
-        shutdown = self._is_shutdown
-        tweets = self._parse_tweet()
+        #sys.stdout.write('STARTED \n')
+        print 'STARTED \n'
+        tweets = self.parse_tweet()
         
         for tweet in tweets:
             tw , created = Tweet.objects.get_or_create(**tweet)
@@ -192,7 +150,8 @@ class TwitterBeat(Thread):
                     object_id       = tw.pk,
                     action_flag     = ADDITION
                 )
-            
+        #sys.stdout.write('Getting Twitter \n')
+        print 'Getting Twitter \n'
         time.sleep(60) # 60 seconds
         
         if self.twitter_user.active and self.KeepAlive and not shutdown.is_set():
